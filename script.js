6b4fd713619a671c509b37d7381d96e11d2f5297
AZheliznyy <zheliznyy@gmail.com>
2023-01-07 11:45:44 +0100

    Start project

---
+++ script.js
@@ -0,0 +1,8 @@
+// Відібрати перший інпут який відповідає за першу дату
+let inputDate = document.querySelector("#input-data-1");
+
+// Відібрати другий інпут який відповідає за другий дату
+let inputDate2 = document.querySelector("#input-data-2");
+
+console.log(inputDate2);
+// він може обрати дату в другому інпуті
